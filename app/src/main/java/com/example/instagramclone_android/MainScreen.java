b1dabd7ffce309ad3b0db8d7a65b1bf9f119d144
Gaurav Nemade <gnemade51@gmail.com>
2020-07-20 14:02:37 +0530

    storyview

--- app/src/main/java/com/example/instagramclone_android/MainScreen.java
+++ app/src/main/java/com/example/instagramclone_android/MainScreen.java
@@ -1,6 +1,8 @@
 package com.example.instagramclone_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.LinearLayout;
@@ -15,6 +17,12 @@ public class MainScreen extends AppCompatActivity {
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_main_screen);
+        RecyclerView recycler_view_story_section = (RecyclerView) findViewById(R.id.recycler_view_story_section);
+        recycler_view_story_section.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        String[] usernames = {"gaurav","nihar","kunal","ajinkya","sakshi","nikita","aaditya","sandeep","mihir"};
+        recycler_view_story_section.setAdapter(new StoryAdapter(usernames));
+
+
     }
 
 
---
+++ app/src/main/java/com/example/instagramclone_android/StoryAdapter.java
@@ -0,0 +1,50 @@
+package com.example.instagramclone_android;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.StoryViewHolder> {
+    @NonNull
+    private String[] data;
+    public StoryAdapter(String[] data)
+    {
+        this.data = data;
+    }
+    @Override
+    public StoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.card_story,parent,false);
+
+        return new StoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoryViewHolder holder, int position) {
+        String username = data[position];
+        holder.storyUsername.setText(username);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public class StoryViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView storyImage;
+        TextView storyUsername;
+        public StoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            storyImage = (CircleImageView) itemView.findViewById(R.id.story_circle_image);
+            storyUsername = (TextView) itemView.findViewById(R.id.story_user_name);
+        }
+    }
+}
