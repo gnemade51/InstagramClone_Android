cd8f89cbeb3a2523a30e0efe8decee3165c64f18
Nihar More <nihar.more33@gmail.com>
2020-07-21 10:40:50 +0530

    image onclick listener added

--- app/src/main/java/com/example/instagramclone_android/MainScreen.java
+++ app/src/main/java/com/example/instagramclone_android/MainScreen.java
@@ -4,30 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainScreen extends AppCompatActivity {
     LinearLayout main_screen_layout;
-
+    ImageView ib1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(getSupportActionBar()!=null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_main_screen);
         RecyclerView recycler_view_story_section = (RecyclerView) findViewById(R.id.recycler_view_story_section);
         recycler_view_story_section.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        String[] usernames = {"gaurav","nihar","kunal","ajinkya","sakshi","nikita","aaditya","sandeep","mihir"};
+        String[] usernames = {"gaurav", "nihar", "kunal", "ajinkya", "sakshi", "nikita", "aaditya", "sandeep", "mihir"};
         recycler_view_story_section.setAdapter(new StoryAdapter(usernames));
 
         RecyclerView recycler_view_post_section = (RecyclerView) findViewById(R.id.recycler_view_post_section);
         recycler_view_post_section.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         recycler_view_post_section.setAdapter(new PostAdapter(usernames));
 
+
+        ib1 = (ImageView) findViewById(R.id.camera);
+
+        ib1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainScreen.this, CameraScreen.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
-
-
 }
+
+
 
