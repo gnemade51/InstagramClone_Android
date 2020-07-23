4f3b289c43723a48a02981a994fb6b7d956caabf
Gaurav Nemade <gnemade51@gmail.com>
2020-07-23 16:54:24 +0530

    Added Camera Feed

--- app/src/main/java/com/example/instagramclone_android/Fragments/PhotoFragment.java
+++ app/src/main/java/com/example/instagramclone_android/Fragments/PhotoFragment.java
@@ -1,13 +1,17 @@
 package com.example.instagramclone_android.Fragments;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.instagramclone_android.R;
+import com.example.instagramclone_android.Utils.ShowCamera;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,34 +19,17 @@ import com.example.instagramclone_android.R;
  * create an instance of this fragment.
  */
 public class PhotoFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Camera camera;
+    FrameLayout frameLayout;
+    ShowCamera showCamera;
 
     public PhotoFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PhotoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PhotoFragment newInstance(String param1, String param2) {
+    public static PhotoFragment newInstance() {
         PhotoFragment fragment = new PhotoFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,16 +37,18 @@ public class PhotoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photo, container, false);
+        View view = inflater.inflate(R.layout.fragment_photo, container, false);
+        frameLayout = (FrameLayout) view.findViewById(R.id.camera_container);
+        //open the camera
+        camera = Camera.open();
+        showCamera = new ShowCamera(getActivity(),camera);
+        frameLayout.addView(showCamera);
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/instagramclone_android/Utils/ShowCamera.java
@@ -0,0 +1,65 @@
+package com.example.instagramclone_android.Utils;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
+    Camera camera;
+    SurfaceHolder holder;
+    public ShowCamera(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        Camera.Parameters params = camera.getParameters();
+
+        List<Camera.Size> sizes = params.getSupportedPictureSizes();
+        Camera.Size mSize = null;
+        for(Camera.Size size : sizes){
+            mSize = size;
+        }
+        //change orientation of camera
+        if(this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE){
+            params.set("orientation","portrait");
+            camera.setDisplayOrientation(90);
+            params.setRotation(90);
+        }else {
+            params.set("orientation","landscape");
+            camera.setDisplayOrientation(0);
+            params.setRotation(0);
+        }
+        params.setPictureSize(mSize.width,mSize.height);
+        camera.setParameters(params);
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+        camera.stopPreview();
+        camera.release();
+    }
+}
